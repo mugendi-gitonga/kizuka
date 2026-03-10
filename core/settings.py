9ec67abf9735762ed6a0b91b808eb542b850d4f7
Mugendi <mugendigitonga@outlook.com>
2026-03-10 15:45:03 +0300

    tls patch

--- core/settings.py
+++ core/settings.py
@@ -258,6 +258,7 @@ EMAIL_PORT = os.environ.get("EMAIL_PORT")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS") == 'True'
+EMAIL_USE_SSL = not EMAIL_USE_TLS
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
 
 
